22a2e5d0df89916a70165ce764e6769d3c8f38ea
borntorun <jmmtcarvalho@gmail.com>
2014-09-30 14:50:00 +0100

    just a second teste w-l

--- server.js
+++ server.js
@@ -1,33 +1,46 @@
 var express = require('express');
 
+
 //#1
 //http = require('http');
+
 
 //variável que determina ambiente
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+
 //criar aplicação Express
 var app = express();
+
+
 /*
 env = string 'development' ou 'production'
     desta forma consegue-se indexar o objecto config directamente para obter apenas
     a configuração para o ambiente desejado
  */
+ 
+ 
 var config = require('./server/config/config')[env];
+
 
 require('./server/config/express')(app, config);
 
+
 require('./server/config/mongoose')(config);
+
 
 require('./server/config/passport')();
 
+
 require('./server/config/routes')(app);
 
+
 app.listen(config.port);
+
 
 //#1
 // http.createServer(app).listen(port);
 
-console.log('A escutar a porta ' + config.port);
 
+console.log('A escutar a porta ' + config.port);
 console.log('...');
